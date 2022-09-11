d4187da7b6c08952147fa497cc4254b5eea2fd7c
JohnCarterGonzalez <gjohncarter@protonmail.com>
2022-09-11 16:09:20 -0700

    draft draft of Product Manager program 2

--- client/src/components/Detail.js
+++ client/src/components/Detail.js
@@ -6,4 +6,26 @@ const Detail = (props) => {
   const [ product, setProduct ] = useState({})
   const {id} = useParams();
 
+  //reach router creats a key:value poar of the props obj
+  //for every vairable found inside the "path" attriubute 
+  //deconstruct the id from props, the unique part of the "path" {:id} 
+  //will have its value assigned in the Link element's "to" attibute 
+  useEffect(() => {
+        axios.get("http://localhost:8000/api/product/" + id)
+            .then(res => {
+                console.log(res.data);
+                setProduct(res.data);
+            })
+            .catch( err => console.log(err) )
+    }, [])
+  return (
+    <div> 
+      <p>Title: {product.title} </p>
+      <p>Price: {product.price} </p>
+      <p>Description: {product.description} </p>
+    </div>
+
+  )
 }
+
+export default Detail;
