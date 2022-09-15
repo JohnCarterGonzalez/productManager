5c0c3b91da16c9543b6a017ae55ede2551212039
JohnCarterGonzalez <gjohncarter@protonmail.com>
2022-09-15 10:49:10 -0700

    first submit

--- client/src/components/ProductList.js
+++ client/src/components/ProductList.js
@@ -1,54 +1,61 @@
 import React, { useEffect } from "react";
-import { Link, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const ProductList = (props) => {
     /* We deconstruct getter and setter which were passed down
-    via props by the parent component (app.js) to our child
-    component (PersonList.js). Now we can easily use the getter
-    and setter without having to write props.getter or props.setter every time: */
+        via props by the parent component (app.js) to our child
+        component (PersonList.js). Now we can easily use the getter
+        and setter without having to write props.getter or props.setter every time: */
 
-  const { removeFromDom, product, setProduct } = props;
-  const nav = useNavigate();
+    const { removeFromDom, product, setProduct } = props;
+    const nav = useNavigate();
 
-  //sending a request to the api to delete the product, also removing it from the DOM
-  const deleteProduct = (productId) => {
-    axios.delete(`http://localhost:8000/api/product/${productId}` )
-      .then( res => {
-        removeFromDom(productId)
-      })
-      .catch( err => console.log(err) )
-  }
-  useEffect(()=> {
-    axios.get('http://localhost:8000/api/product')
-      .then((res) => {
-        console.log(res.data); 
-        setProduct(res.data);
-      })
-  }, [])
+    //sending a request to the api to delete the product, also removing it from the DOM
+    const deleteProduct = (productId) => {
+        axios
+            .delete(`http://localhost:8000/api/product/${productId}`)
+            .then((res) => {
+                removeFromDom(productId);
+            })
+            .catch((err) => console.log(err));
+    };
+    useEffect(() => {
+        axios.get("http://localhost:8000/api/product").then((res) => {
+            console.log(res.data);
+            setProduct(res.data);
+        });
+    }, []);
 
-  return(
-    <div> 
-      <h1>All Products</h1>
-      <br/>
-      {
-        product.map((product, index)=> {
-          return (
-            <div key={ index }>
-              <Link to={ "/product/" + product._id}> 
-                Title: {product.title}
-                <br />
-                Price: {product.price} 
-                <br/>
-              </Link>
-            <button onClick={ (e) => nav(`/product/edit/${product._id}`) }>Edit</button>
-              <br />
-            <button onClick={ (e) => {deleteProduct(product._id)} }>Delete</button>
-            </div>
-        )})
-      }
-    </div>
-  )
-} 
+    return (
+        <div>
+            <h1>All Products</h1>
+            <br />
+            {product.map((product, index) => {
+                return (
+                    <div key={index}>
+                        <Link to={"/product/" + product._id}>
+                            Title: {product.title}
+                            <br />
+                            Price: {product.price}
+                            <br />
+                        </Link>
+                        <button onClick={(e) => nav(`/product/edit/${product._id}`)}>
+                            Edit
+                        </button>
+                        <br />
+                        <button
+                            onClick={(e) => {
+                                deleteProduct(product._id);
+                            }}
+                        >
+                            Delete
+                        </button>
+                    </div>
+                );
+            })}
+        </div>
+    );
+};
 
 export default ProductList;
--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,22 +1,56 @@
-import React, { useState } from "react";
-import ProductList from '../components/ProductList';
-import ProductForm from '../components/ProductForm';
+import React, { useEffect, useState } from "react";
+import ProductList from "../components/ProductList";
+import ProductForm from "../components/ProductForm";
+import axios from "axios";
 
 const Main = (props) => {
-  const [ product, setProduct ] = useState([]);
+    const [product, setProduct] = useState([]);
 
-  const removeFromDom = productId => {
-    setProduct(product.filter(product => product._id !== productId));
-  }
-  
-  /* PersonForm and Person List can both utilize the getter and setter established in their parent component: */
-  return (
-    <div>
-      <ProductForm product={ product } setProduct={ setProduct } />
-      <hr/>
-      <ProductList product={ product } setProduct={ setProduct } removeFromDom={removeFromDom}/>
-    </div>
-  )
-}
+    useEffect(() => {
+        axios
+            .get("http://localhost:8000/api/product")
+            .then((res) => {
+                setProduct(res.data);
+            })
+            .catch((err) => console.log(err));
+    }, []);
+
+    //deletion method, takes an id as a param, then once fulfilled, displays all the products where the id does not
+    //match
+    const removeFromDom = (productId) => {
+        axios
+            .delete("http://localhost:8000/api/product/" + productId)
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                setProduct(product.filter((product) => product._id !== productId));
+            });
+    };
+
+    //the creation method takes the product as a body and makes a request to the url, setting it into the product array
+    const createProduct = (productParam) => {
+        axios
+            .post("http://localhost:8000/api/product", productParam)
+            .then((res) => {
+                console.log(res);
+                console.log(res.data);
+                setProduct([...product, res.data]);
+            })
+            .catch((err) => console.log(err));
+    };
+
+    /* PersonForm and Person List can both utilize the getter and setter established in their parent component: */
+    return (
+        <div>
+            <ProductForm
+                product={product}
+                setProduct={setProduct}
+                onSubmitProp={createProduct}
+            />
+            <hr />
+            <ProductList product={product} removeFromDom={removeFromDom} />
+        </div>
+    );
+};
 
 export default Main;
